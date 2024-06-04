09832450915a65257a0eca3e22e890ce9ee08ee6
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-04 23:24:31 +0530

    Create num_of_digits.cpp

---
+++ Week2_Recursion1/Number of digit/num_of_digits.cpp
@@ -0,0 +1,44 @@
+/*
+NUMBER OF DIGITS
+
+Problem statement
+You are given a number 'n'.
+
+Return number of digits in ‘n’.
+
+Constraints:
+1 <= ‘n’ <= 10^9
+
+Example:
+Input: 'n' = 123
+
+Output: 3
+
+Explanation:
+The 3 digits in ‘123’ are 1, 2 and 3. 
+
+----------------------------------------
+Sample Input 1:
+121
+Sample Output 1:
+3
+----------------------------------------
+
+  
+----------------------------------------
+Sample Input 2:
+38
+Sample Output 2:
+2
+----------------------------------------
+*/
+
+
+int countDigits(int n){
+	if (n == 0) {
+        return 1;
+    }
+    return floor(log10(abs(n))) + 1;
+}
+
+
