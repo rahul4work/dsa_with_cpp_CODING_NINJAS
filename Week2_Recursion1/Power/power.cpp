d7064b575b7a543de3dd802acbc16368cfcc0e2f
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-05 23:57:48 +0530

    Create power.cpp

---
+++ Week2_Recursion1/Power/power.cpp
@@ -0,0 +1,53 @@
+/*
+Problem statement
+Write a program to find x to the power n (i.e. x^n). Take 
+x and n from the user. You need to return the answer.
+
+Do this recursively.
+
+Constraints :
+0 <= x <= 30
+0 <= n <= 30
+
+  
+---------------------------------------------------------
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+---------------------------------------------------------
+
+
+---------------------------------------------------------
+Sample Input 2 :
+ 2 5
+Sample Output 2 :
+32
+---------------------------------------------------------
+*/
+
+#include<iostream>
+#include "Solution.h"
+using namespace std;
+
+int power(int x, int n) {
+
+  if (n == 0) {
+        return 1;
+    }
+    if (n > 0) {
+        return x * power(x, n - 1);
+    }
+    return 1 / power(x, -n);
+
+}
+
+
+int main(){
+    int x, n;
+    cin >> x >> n;
+  
+    cout << power(x, n) << endl;
+}
+
+
