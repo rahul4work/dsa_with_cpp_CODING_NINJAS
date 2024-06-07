fdbf09072dfb1b99b167d1348a81738de015b3dc
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-07 23:41:00 +0530

    Update num_of_digits.cpp

--- Week2_Recursion1/Number of digit/num_of_digits.cpp
+++ Week2_Recursion1/Number of digit/num_of_digits.cpp
@@ -34,6 +34,10 @@ Sample Output 2:
 */
 
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
 int countDigits(int n){
 	if (n == 0) {
         return 1;
@@ -41,4 +45,13 @@ int countDigits(int n){
     return floor(log10(abs(n))) + 1;
 }
 
+int main() {
 
+    int n;
+    cin>>n;
+
+    cout<<countDigits(n) <<endl;
+    
+
+    return 0;
+}
