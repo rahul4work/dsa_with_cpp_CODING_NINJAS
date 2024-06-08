bcdc7debc4502e7af3d9cb1f8e146bd97cc76b5e
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-09 00:07:02 +0530

    Update multiplication.cpp

--- Week2_Recursion1/Multiplication (recursive)/multiplication.cpp
+++ Week2_Recursion1/Multiplication (recursive)/multiplication.cpp
@@ -1,21 +1,33 @@
 /*
-Problem statement
-Given two integers M & N, calculate and return their multiplication using recursion. You can only use subtraction and addition for your calculation. No other operators are allowed.
+MULTIPLICATION (RECURSIVE)
 
-Detailed explanation ( Input/output format, Notes, Images )
+Problem statement
+Given two integers M & N, calculate and return their multiplication 
+using recursion. You can only use subtraction and addition for your 
+calculation. No other operators are allowed.
+
+
 Constraints :
 0 <= M <= 1000
 0 <= N <= 1000
+
+
+--------------------------------------------------------------------
 Sample Input 1 :
 3 
 5
 Sample Output 1 :
 15
+--------------------------------------------------------------------
+
+
+--------------------------------------------------------------------
 Sample Input 2 :
 4 
 0
 Sample Output 2 :
 0
+-------------------------------------------------------------------
 */
 
 #include <iostream>
@@ -23,7 +35,6 @@ Sample Output 2 :
 using namespace std;
 
 int multiplyNumbers(int m, int n) {
-    // Write your code here
 
     if (m == 0 || n == 0) {
         return 0;
@@ -41,10 +52,10 @@ int multiplyNumbers(int m, int n) {
 }
 
 
-
-
 int main() {
     int m, n;
     cin >> m >> n;
     cout << multiplyNumbers(m, n) << endl;
 }
+
+
