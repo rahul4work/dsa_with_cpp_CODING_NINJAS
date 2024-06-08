1820bac8cbae5c6030ddab857efc3ddf137aba74
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-08 23:59:10 +0530

    Create sum_of_digits.cpp

---
+++ Week2_Recursion1/Sum of digits (recursive)/sum_of_digits.cpp
@@ -0,0 +1,41 @@
+/*
+Problem statement
+Write a recursive function that returns the sum of the digits of a given integer.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+0 <= N <= 10^9
+Sample Input 1 :
+12345
+Sample Output 1 :
+15
+Sample Input 2 :
+9
+Sample Output 2 :
+9
+*/
+
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+int sumOfDigits(int n) {
+    // Write your code here
+
+    if (n < 10) {
+        return n;
+    }
+    int lastDigit = n % 10;
+    int sumOfRest = sumOfDigits(n / 10);
+    
+    return lastDigit + sumOfRest;
+}
+
+
+
+
+int main() {
+   int n;
+   cin >> n;
+   cout << sumOfDigits(n) << endl;
+}
