3e1d10e15843043c7cdfb9dd3e70b5e368da174b
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-22 20:40:35 +0530

    Create sort_0_1_2.cpp

---
+++ Week3_Time_and_Space_Complexity_analysis/Sort 0 1 2/sort_0_1_2.cpp
@@ -0,0 +1,102 @@
+/*
+SORT 0 1 2
+
+Problem statement
+You are given an integer array/list(ARR) of size N. It contains only 0s, 1s and 2s. Write a solution to 
+sort this array/list in a 'single scan'.
+
+'Single Scan' refers to iterating over the array/list just once or to put it in other words, you will
+be visiting each element in the array/list just once.
+
+Note:
+1. You need to change in the given array/list itself. Hence, no need to return or print anything. 
+2. You are not allowed to sort the list/array directly.
+
+
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+Time Limit: 1 sec
+
+
+------------------------------------------------------------------------------------------------------
+Sample Input 1:
+1
+7
+0 1 2 0 2 0 1
+Sample Output 1:
+0 0 0 1 1 2 2 
+------------------------------------------------------------------------------------------------------
+  
+
+------------------------------------------------------------------------------------------------------
+Sample Input 2:
+2
+5
+2 2 0 1 1
+7
+0 1 2 0 1 2 0
+Sample Output 2:
+0 1 1 2 2 
+0 0 0 1 1 2 2
+------------------------------------------------------------------------------------------------------
+*/
+
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void sort012(int *arr, int n)
+{
+    //Write your code here
+    int low = 0, mid = 0, high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        } else if (arr[mid] == 1) {
+            mid++;
+        } else { 
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int size;
+
+		cin >> size;
+		int *arr = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> arr[i];
+		}
+
+		sort012(arr, size);
+
+		for (int i = 0; i < size; i++)
+		{
+			cout << arr[i] << " ";
+		}
+
+		delete[] arr;
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
+
+
