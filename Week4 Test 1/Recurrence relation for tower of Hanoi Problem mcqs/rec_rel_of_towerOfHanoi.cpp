9a2dc64b89fcc3589af3cf63f3e756c9ef139b17
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-23 22:52:28 +0530

    Create rec_rel_of_towerOfHanoi.cpp

---
+++ Week4 Test 1/Recurrence relation for tower of Hanoi Problem mcqs/rec_rel_of_towerOfHanoi.cpp
@@ -0,0 +1,13 @@
+Problem statement
+The recurrence relation capturing the optimal execution time of the Towers of Hanoi problem with n discs is :
+
+
+Options: One or more answers may be correct
+• T(n) = 2T(n - 2) + 2
+• T(n) = 2T(n - 1) + n
+• T(n) = 2T(n/2) + 1
+• T(n) = 2T(n - 1) + 1
+
+
+answer:-
+T(n) = 2T(n - 1) + 1
