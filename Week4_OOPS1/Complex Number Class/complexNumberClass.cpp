63bf59c18a8db0965e13338b40c973a627d5201a
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-06-28 23:38:20 +0530

    Update complexNumberClass.cpp

--- Week4_OOPS1/Complex Number Class/complexNumberClass.cpp
+++ Week4_OOPS1/Complex Number Class/complexNumberClass.cpp
@@ -1,5 +1,6 @@
 /*
 COMPLEX NUMBER CLASS
+level: moderate
 
 Problem statement
 A ComplexNumber class contains two data members : one is the real part (R) and the other is imaginary (I) (both integers).
