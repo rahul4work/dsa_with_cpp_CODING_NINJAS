22b4d37c611a07482565b78d6e8d0c2d73c5c65c
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-07-06 14:16:32 +0530

    Create merge_sorted_linked_list.cpp

---
+++ Week6_Linked_List2/Merge Two Sorted Linked List/merge_sorted_linked_list.cpp
@@ -0,0 +1,147 @@
+/*
+MERGE TWO SORTED LINKED LIST
+
+Problem statement
+You have been given two sorted(in ascending order) singly linked lists of integers.
+
+Write a function to merge them in such a way that the resulting singly linked list
+is also sorted(in ascending order) and return the new head to the list.
+
+Note :
+Try solving this in O(1) auxiliary space.
+
+No need to print the list, it has already been taken care.
+
+Constraints :
+1 <= t = 10^2
+0 <= N <= 10 ^ 4
+0 <= M <= 10 ^ 4
+Where N and M denote the sizes of the singly linked lists. 
+
+Time Limit: 1sec
+
+
+------------------------------------------------------------------------------------
+Sample Input 1 :
+1
+2 5 8 12 -1
+3 6 9 -1
+Sample Output 1 :
+2 3 5 6 8 9 12 
+------------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------------
+Sample Input 2 :
+2
+2 5 8 12 -1
+3 6 9 -1
+10 40 60 60 80 -1
+10 20 30 40 50 60 90 100 -1
+Sample Output 2 :
+2 3 5 6 8 9 12 
+10 10 20 30 40 40 50 60 60 60 80 90 100
+------------------------------------------------------------------------------------
+*/
+
+
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
+{
+    //Write your code here
+	if (!head1) return head2;
+    if (!head2) return head1;
+
+    Node *dummy = new Node(0);
+    Node *tail = dummy;
+
+    while (head1 && head2) {
+        if (head1->data <= head2->data) {
+            tail->next = head1;
+            head1 = head1->next;
+        } else {
+            tail->next = head2;
+            head2 = head2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (head1) {
+        tail->next = head1;
+    } else {
+        tail->next = head2;
+    }
+
+
+    Node *mergedHead = dummy->next;
+
+    delete dummy;
+
+    return mergedHead;
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head1 = takeinput();
+		Node *head2 = takeinput();
+		Node *head3 = mergeTwoSortedLinkedLists(head1, head2);
+		print(head3);
+	}
+	return 0;
+}
+
+
