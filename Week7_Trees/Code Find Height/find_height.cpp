8c807be7beeac2e82db2904f65eef05bdd90cd08
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-07-31 22:28:51 +0530

    Create find_height.cpp

---
+++ Week7_Trees/Code Find Height/find_height.cpp
@@ -0,0 +1,90 @@
+/*
+HEIGHT OF GENERIC TREE
+
+Problem statement
+Given a generic tree, find and return the height of given tree. The height of a t
+ree is defined as the longest distance from root node to any of the leaf node. Assume 
+the height of a tree with a single node is 1.
+
+
+Constraints:
+Time Limit: 1 sec
+
+
+---------------------------------------------------------------------------------------
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1:
+3
+---------------------------------------------------------------------------------------
+*/
+
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+int getHeight(TreeNode<int>* root) {
+    // Write your code here
+    if (root == nullptr) {
+        return 0;
+    }
+
+    int maxHeight = 0;
+    for (int i = 0; i < root->children.size(); i++) {
+        int childHeight = getHeight(root->children[i]);
+        if (childHeight > maxHeight) {
+            maxHeight = childHeight;
+        }
+    }
+
+    return maxHeight + 1;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << getHeight(root);
+}
+
+
