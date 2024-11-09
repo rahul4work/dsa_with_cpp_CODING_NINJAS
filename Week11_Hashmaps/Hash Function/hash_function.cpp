6e4aadad5c102d9e511e839fa645d34a8e542093
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-11-09 17:14:01 +0530

    Create hash_function.cpp

---
+++ Week11_Hashmaps/Hash Function/hash_function.cpp
@@ -0,0 +1,13 @@
+HASH FUNCTION
+
+Problem statement
+What is the role of a hash function in a hashmap?
+
+
+Options: Pick one correct answer from below
+• To calculate the size of the hashmap
+• To determine the hashcode which in turns helps us to determine the index where an element will be stored
+• To arrange the elements in the hashmap in a particular order
+• To eliminate duplicate elements from the hashmap
+
+ans: To determine the hashcode which in turns helps us to determine the index where an element will be stored
